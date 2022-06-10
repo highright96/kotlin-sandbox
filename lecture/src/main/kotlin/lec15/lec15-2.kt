f8b753d03cd4b417cf7bc11056c14b7fef2cac6a
highright96 <nsw5296@naver.com>
2022-06-10 18:18:46 +0900

    add: lec15 추가

---
+++ lecture/src/main/kotlin/lec15/lec15-2.kt
@@ -0,0 +1,28 @@
+package lec15
+
+fun main() {
+
+    //불변 리스트 생성
+    val numbers = listOf(100, 200)
+
+    //가변 리스트 생성
+    val mutableNumbers = mutableListOf(100, 200)
+
+    /*
+    비어있는 컬렉션 생성
+    아직 값을 넣어주지 않아서 타입을 명시적으로 적어줘야 한다.
+    */
+    val emptyList = emptyList<Int>()
+
+    println(numbers[0])
+
+    for (number in numbers) {
+        println(number)
+    }
+
+    for ((idx, value) in numbers.withIndex()) {
+        println("$idx : $value")
+    }
+
+    val setNumbers = setOf(100)
+}
