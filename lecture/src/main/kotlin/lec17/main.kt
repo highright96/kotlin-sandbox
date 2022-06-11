122c8fd992310d28eb0ec987709a89129f142049
highright96 <nsw5296@naver.com>
2022-06-12 00:10:53 +0900

    add: lec18 추가

--- lecture/src/main/kotlin/lec17/main.kt
+++ lecture/src/main/kotlin/lec17/main.kt
@@ -46,14 +46,7 @@ fun main() {
 }
 
 private fun filterFruits(
-    fruits: List<Fruit>,
-    filter: (Fruit) -> Boolean
+    fruits: List<Fruit>, filter: (Fruit) -> Boolean
 ): List<Fruit> {
-    val results = mutableListOf<Fruit>()
-    for (fruit in fruits) {
-        if (filter(fruit)) {
-            results.add(fruit)
-        }
-    }
-    return results
+    return fruits.filter(filter)
 }
---
+++ lecture/src/main/kotlin/lec18/FlatMap.kt
@@ -0,0 +1,31 @@
+package lec18
+
+fun main() {
+    val fruitsInLIst: List<List<Fruit>> = listOf(
+        listOf(
+            Fruit(1, "사과", 1_000),
+            Fruit(1, "사과", 1_200),
+            Fruit(1, "사과", 1_500),
+            Fruit(1, "바나나", 3_000),
+        ),
+        listOf(
+            Fruit(1, "사과", 1_000),
+            Fruit(1, "사과", 1_200),
+            Fruit(1, "사과", 1_500),
+        )
+    )
+
+    //출고가와 현재가가 동일한 과일
+    //flatMap : 중첩 리스트를 여러 조건을 포함한 단일 리스트로 변경
+    val samePriceFruits1 = fruitsInLIst.flatMap { list ->
+        list.filter { fruit -> fruit.factoryPrice == fruit.currentPrice }
+    }
+    val samePriceFruits2 = fruitsInLIst.flatMap { list -> list.samePriceFilter }
+
+    //flatten : 중첩 리스트를 단일 리스트로 변경
+    val flattenList = fruitsInLIst.flatten()
+
+}
+
+val List<Fruit>.samePriceFilter: List<Fruit>
+    get() = this.filter(Fruit::isSamePrice)
